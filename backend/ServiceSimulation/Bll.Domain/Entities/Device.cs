75347b307398b203713f9666c86634dcf3425bc3
NiceOneFox <kulakovskij.es@edu.spbstu.ru>
2022-04-18 19:17:10 +0300

    Add Device

---
+++ backend/ServiceSimulation/Bll.Domain/Entities/Device.cs
@@ -0,0 +1,41 @@
+﻿using Bll.Domain.Interfaces;
+using Bll.Domain.Services;
+
+namespace Bll.Domain.Entities;
+
+public class Device : IDevice
+{
+    public int DeviceId { get; set; }
+    public bool IsWorking { get; set; }
+    public Request? Request { get; set; }
+    private readonly ITimeProvider _time;
+    public Device(int deviceId, bool isWorking, Request? request, ITimeProvider time)
+    {
+        DeviceId = deviceId;
+        IsWorking = isWorking;
+        Request = request;
+        _time = time;
+    }
+
+    public void TakeRequest(Request request)
+    {
+        if (IsWorking)
+        {
+            return;
+        }
+        Request = request;
+
+        //TODO COUNT TIME OF WORKING ON THAT REQUEST
+    }
+
+    public bool IsFree()
+    {
+        if (Request?.EndTime <= _time.Now)
+        {
+            Request = null;
+            IsWorking = false;
+            // TODO COUNT THAT REQUEST WHICH IS END WORKING.
+        }
+        return true;
+    }
+}
---
+++ backend/ServiceSimulation/Bll.Domain/Services/IDevice.cs
@@ -0,0 +1,9 @@
+﻿using Bll.Domain.Entities;
+
+namespace Bll.Domain.Services;
+
+public interface IDevice
+{
+    void TakeRequest(Request request);
+    bool IsFree();
+}

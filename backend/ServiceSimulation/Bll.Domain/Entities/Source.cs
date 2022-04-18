d21b0d6b0c596f09b2b2c9e7da8d3addf27da166
NiceOneFox <kulakovskij.es@edu.spbstu.ru>
2022-04-18 16:18:34 +0300

    git implement method. Delete Comments.

--- backend/ServiceSimulation/Bll.Domain/Entities/Source.cs
+++ backend/ServiceSimulation/Bll.Domain/Entities/Source.cs
@@ -1,4 +1,5 @@
-﻿using Bll.Domain.Services;
+﻿using Bll.Domain.Interfaces;
+using Bll.Domain.Services;
 
 namespace Bll.Domain.Entities;
 
@@ -7,80 +8,19 @@ public class Source : ISource
     public int SourceId { get; set; }
     public int Priority { get; set; }
     public int SerialNumber { get; set; }
+    public ITimeProvider Time { get; set; }
+    public Source(int sourceId, int priority, int serialNumber, ITimeProvider time)
+    {
+        SourceId = sourceId;
+        Priority = priority;
+        SerialNumber = serialNumber;
+        Time = time;
+    }
+
     public Request GetNewRequest()
     {
-        var generatedRequest = new Request(SourceId, SerialNumber, DateTime.Now, DateTime.Now.AddMilliseconds(5.0));
+        var generatedRequest = new Request(SourceId, SerialNumber, Time.Now, DateTime.Now.AddMilliseconds(5.0));
         SerialNumber++;
         return generatedRequest;
     }
 }
-
-class Source
-{
-    public Request sourceRequests { get; set; }
-    public int numberSource { get; set; }
-    public int serialNumber { get; set; } = 0;
-
-    public int priority { get; set; } = 0;
-
-    public int declined { get; set; } = 0;
-
-    public float Tpreb { get; set; } = 0;
-
-    public float TObcl { get; set; } = 0;
-
-    public float Tbp { get; set; } = 0;
-
-    public int TbpAmount { get; set; } = 0; // кол-во заявок в буфере этого источника
-
-    public List<float> allRequetsOfSource { get; set; }
-
-    public List<float> allTbp { get; set; }
-
-    public Source(int numberSource, int serialNumber, int priority)
-    {
-        this.numberSource = numberSource;
-        this.serialNumber = serialNumber;
-        this.priority = priority;
-        this.declined = 0;
-        this.TObcl = 0;
-        this.Tbp = 0;
-        allRequetsOfSource = new List<float>();
-        allTbp = new List<float>();
-        // random first request
-        sourceRequests = new Request(Program.time + ((float)Program.random.NextDouble()) / 100, numberSource, serialNumber);
-        serialNumber++;
-        allRequetsOfSource.Add(sourceRequests.dateTime);
-    }
-
-    public Source() { }
-
-    public float GetTimeRequest()
-    {
-        if (sourceRequests == null) return -1;
-        return sourceRequests.dateTime;
-    }
-
-    public Request GetNewRequest() // отдаём текущую сгенирированную и генерируем новую
-    {
-        Request temp = this.sourceRequests;
-        float prevTime = temp.dateTime;
-
-        serialNumber++;
-        sourceRequests = null;
-        // генерировать следующую заявку источника           
-        sourceRequests = new Request(prevTime + Program.timeOfSourceGenerateNewRequest, numberSource, serialNumber);
-
-        Program.numberOfRequestsWereGenerated++;
-
-        if (Program.numberOfRequestsWereGenerated >= Program.totalRequestGenerate)
-        {
-            sourceRequests = null;
-            // больше не генерируем
-        }
-
-        Program.allRequestInSystem.Add(temp);
-        allRequetsOfSource.Add(temp.dateTime);
-        // возращаем текущую
-        return temp;
-    }

42dd7200d08df20eecf6574e466a1e5fb6e331de
NiceOneFox <kulakovskij.es@edu.spbstu.ru>
2022-04-24 17:39:51 +0300

    Delete comments

--- backend/ServiceSimulation/Bll.Domain/Services/SimulationService.cs
+++ backend/ServiceSimulation/Bll.Domain/Services/SimulationService.cs
@@ -66,66 +66,6 @@ public class SimulationService : ISimulationService
             }
             FindNextSpecialEvent(devices, sources, bufferManager, parameters);
         }
-
-
-        //while (_results.AmountOfServedRequest >= parameters.AmountOfRequests &&
-        //       !bufferManager.IsFree() &&
-        //       !IsAllDevicesFree(devices))
-        //{
-        //    // TODO CHECK FOR NEXT SPECIAL EVENT
-        //    double timeOfClosestRequestCome = double.MaxValue;
-        //    double timeOfClosestDeviceFree = double.MaxValue;
-
-        //    // Check requests
-        //    int indexOfRequestWithClosestTimeCome = -1;
-        //    for (int i = 0; i < sources.Count; i++)
-        //    {
-        //        if (sources[i].TimeOfNextRequest < timeOfClosestRequestCome)
-        //        {
-        //            timeOfClosestRequestCome = sources[i].TimeOfNextRequest;
-        //            indexOfRequestWithClosestTimeCome = i;
-        //        }
-        //    }
-
-        //    int indexOfDeviceWithClosestTimeFree = -1;
-        //    for (int i = 0; i < devices.Count; i++)
-        //    {
-        //        if (devices[i].TimeOfDeviceWillBeFree < indexOfDeviceWithClosestTimeFree)
-        //        {
-        //            timeOfClosestDeviceFree = devices[i].TimeOfDeviceWillBeFree;
-        //            indexOfDeviceWithClosestTimeFree = i;
-        //        }
-        //    }
-
-        //    if (timeOfClosestDeviceFree < timeOfClosestRequestCome)
-        //    {
-        //        _deviceManager.FreeDevice(devices[indexOfDeviceWithClosestTimeFree]);
-
-        //        // Take from buffer and put on device.
-        //        _deviceManager.TakeRequest(bufferManager.Get(), devices[indexOfDeviceWithClosestTimeFree]);
-        //    }
-        //    else
-        //    {
-        //        // Take request on work or add to buffer.
-        //        var newRequestInSystem = _sourceManager.GetNewRequest(sources[indexOfRequestWithClosestTimeCome]);
-        //        // find out free device
-        //        bool wasFreeDevice = false;
-        //        foreach (var device in devices)
-        //        {
-        //            if (!device.IsWorking)
-        //            {
-        //                _deviceManager.TakeRequest(newRequestInSystem, device);
-        //                wasFreeDevice = true;
-        //            }
-        //        }
-
-        //        if (!wasFreeDevice)
-        //        {
-        //            // Add request to buffer if no free devices were found
-        //            bufferManager.Add(newRequestInSystem);
-        //        }
-        //    }
-        //}
     }
 
     private void FindNextSpecialEvent(List<Device> devices, List<Source> sources, IBufferManager bufferManager, InputParameters parameters)

5fd6f729f1eb446fd8f64b89a7e2842d51c51c64
NiceOneFox <kulakovskij.es@edu.spbstu.ru>
2022-05-01 13:57:30 +0300

    Add factory and implementations

---
+++ backend/ServiceSimulation/Bll.Domain/Interfaces/IDeviceDirector.cs
@@ -0,0 +1,10 @@
+﻿
+using Bll.Domain.Models;
+
+namespace Bll.Domain.Interfaces
+{
+    public interface IDeviceDirector
+    {
+        Device? ChooseDevice(IEnumerable<Device> devices, IDeviceManager deviceManager);
+    }
+}

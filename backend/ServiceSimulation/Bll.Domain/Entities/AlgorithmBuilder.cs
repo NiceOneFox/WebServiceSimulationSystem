1f26aa7973e9350801a67a74aea1940bd2481b94
NiceOneFox <kulakovskij.es@edu.spbstu.ru>
2022-05-02 22:01:45 +0300

    Add Algorithm Builder pattern for construct algorithm

---
+++ backend/ServiceSimulation/Bll.Domain/Entities/AlgorithmBuilder.cs
@@ -0,0 +1,32 @@
+﻿using Bll.Domain.Interfaces;
+
+namespace Bll.Domain.Entities
+{
+    public class AlgorithmBuilder
+    {
+        private Algorithm _algorithm = new();
+
+        public AlgorithmBuilder WithDeviceManager(IDeviceManager deviceManager)
+        {
+            _algorithm.deviceManager = deviceManager;
+            return this;
+        }
+
+        public AlgorithmBuilder WithSourceManager(ISourceManager sourceManager)
+        {
+            _algorithm.sourceManager = sourceManager;
+            return this;
+        }
+
+        public AlgorithmBuilder WithBufferManager(IBufferManager bufferManager)
+        {
+            _algorithm.bufferManager = bufferManager;
+            return this;
+        }
+
+        public Algorithm Build()
+        {
+            return _algorithm;
+        }
+    }
+}
